c6eae2d7d16899f4fae4209e3a86a3546ebe94d5
MonkP <scloolk@outlook.com>
2019-05-22 22:30:19 +0800

    增加Session配置

--- HomeProject.Backend.Server/Controllers/HomeController.cs
+++ HomeProject.Backend.Server/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using HomeProject.Backend.Models.Dto;
+using HomeProject.Backend.Models.Dtos;
 
 namespace HomeDemo.Backend.Server.Controllers
 {
--- HomeProject.Backend.Server/Startup.cs
+++ HomeProject.Backend.Server/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using HomeProject.Backend.Models.DB;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -30,9 +31,22 @@ namespace HomeProject.Backend
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
+            //为Session配置使用缓存
+            services.AddDistributedMemoryCache();
+            //配置Session
+            services.AddSession(options =>
+            {
+                // Set a short timeout for easy testing.
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                // Make the session cookie essential
+                options.Cookie.IsEssential = true;
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //初始化数据库连接
+            services.AddDbContext<HomeDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,7 +62,7 @@ namespace HomeProject.Backend
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseSession();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
