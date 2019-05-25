b58049f3519dd0eeae4bbea19d094cf206d8a3f5
MonkP <scloolk@outlook.com>
2019-05-26 00:44:21 +0800

    增加DB访问基类

---
+++ HomeProject.Backend.DAL/BaseService.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.EntityFrameworkCore.Extensions;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Transactions;
+
+namespace HomeProject.Backend.DAL
+{
+    public class BaseService<T> where T : class, new()
+    {
+        public DbContext db = EFContextFactory.GetCurrentDbContext();
+        #region 查询方法
+        /// <summary>
+        /// 单体查询
+        /// </summary>
+        /// <param name="whereExp">查询条件表达式</param>
+        /// <returns></returns>
+        public virtual T Find(Expression<Func<T,bool>> whereExp)
+        {
+            return db.Set<T>().FirstOrDefault(whereExp);
+        }
+        public virtual bool Exist(Expression<Func<T, bool>> whereExp)
+        {
+            return db.Set<T>().Count(whereExp) != 0;
+        }
+        /// <summary>
+        /// 普通批量查询
+        /// </summary>
+        /// <param name="whereExp">查询条件表达式</param>
+        /// <returns></returns>
+        public virtual IQueryable<T> GetEntities(Expression<Func<T, bool>> whereExp)
+        {
+            return db.Set<T>().Where<T>(whereExp).AsQueryable();
+        }
+        /// <summary>
+        /// 分页查询
+        /// </summary>
+        /// <param name="wherelambda">查询条件表达式</param>
+        /// <param name="pageNumber">页码，从1开始</param>
+        /// <param name="pageSize">页容量</param>
+        /// <param name="sortProperty">排序属性</param>
+        /// <param name="isDesc">是否降序排列</param>
+        /// <returns></returns>
+        public virtual IQueryable<T> GetEntities(Expression<Func<T, bool>> wherelambda, int pageNumber, int pageSize, string sortProperty, bool isDesc = false)
+        {
+            var tempEntities = GetEntities(wherelambda);
+            var sortDirection = isDesc ? "DSEC" : "ASC";
+            return DataSortingAndPaging<T>(tempEntities, sortProperty, sortDirection, pageNumber, pageSize);
+        }
+
+        /// <summary>
+        /// 对查询结果，分页排序方法
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="sortExpression"></param>
+        /// <param name="sortDirection"></param>
+        /// <param name="pageNumber"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public virtual IQueryable<T> DataSortingAndPaging<T>(IQueryable<T> source, string sortExpression, string sortDirection, int pageNumber, int pageSize)
+        {
+            //先排序后分页
+            IQueryable<T> query = DataSorting<T>(source, sortExpression, sortDirection);
+            return DataPaging(query, pageNumber, pageSize);
+        }
+        /// <summary>
+        /// 对查询结果，排序方法
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">待排序的原始数据集</param>
+        /// <param name="sortProperty">排序属性名</param>
+        /// <param name="sortDirection"></param>
+        /// <returns></returns>
+        public virtual IQueryable<T> DataSorting<T>(IQueryable<T> source, string sortProperty, string sortDirection)
+        {
+            string sortingDir = string.Empty;
+            if (sortDirection.ToUpper().Trim() == "ASC")
+                sortingDir = "OrderBy";
+            else if (sortDirection.ToUpper().Trim() == "DESC")
+                sortingDir = "OrderByDescending";
+            ParameterExpression param = Expression.Parameter(typeof(T), sortProperty);
+            PropertyInfo pi = typeof(T).GetProperty(sortProperty);
+            Type[] types = new Type[2];
+            types[0] = typeof(T);
+            types[1] = pi.PropertyType;
+            Expression expr = Expression.Call(typeof(Queryable), sortingDir, types, source.Expression, Expression.Lambda(Expression.Property(param, sortProperty), param));
+            IQueryable<T> query = source.AsQueryable().Provider.CreateQuery<T>(expr);
+            return query;
+        }
+
+        /// <summary>
+        /// 对查询结果，分页方法
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">待分页的原始数据集</param>
+        /// <param name="pageNumber">从1开始</param>
+        /// <param name="pageSize">页容量</param>
+        /// <returns></returns>
+        public virtual IQueryable<T> DataPaging<T>(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            return source.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+        }
+        #endregion
+
+        #region Raw SQL 操作
+        /// <summary>
+        /// 执行查询语句
+        /// </summary>
+        /// <param name="querySql">SQL语句</param>
+        /// <param name="par">参数集合，可为空</param>
+        /// <returns>查询集合</returns>
+        public IQueryable<T> ExcuteSqlQuery(string querySql, params object[] par)
+        {
+            return db.Set<T>().FromSql<T>(querySql, par).AsQueryable();
+        }
+        /// <summary>
+        /// 执行更新、删除语句
+        /// </summary>
+        /// <param name="sqlCommand">SQL语句</param>
+        /// <param name="par">参数集合，可为空</param>
+        /// <returns></returns>
+        public int ExcuteSqlCommand(string sqlCommand,params object[] par)
+        {
+            return db.Database.ExecuteSqlCommand(sqlCommand, par);
+        }
+        /// <summary>
+        /// 异步执行更新、删除语句
+        /// </summary>
+        /// <param name="sqlCommand">SQL语句</param>
+        /// <param name="par">参数集合，可为空</param>
+        /// <returns></returns>
+        public Task<int> ExcuteSqlCommandAsync(string sqlCommand,params object[] par)
+        {
+            return db.Database.ExecuteSqlCommandAsync(sqlCommand, par);
+        }
+
+        #endregion
+        /// <summary>
+        /// 字段赋值方法
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="K"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="propertyName"></param>
+        /// <param name="val"></param>
+        private void SetValue<T, K>(T obj, string propertyName, K val)
+        {
+            PropertyInfo propertyInfo;
+            propertyInfo = typeof(T).GetProperty(propertyName, BindingFlags.GetProperty | BindingFlags.Instance | BindingFlags.Public);
+            if (propertyInfo != null)
+            {
+                Type type = Nullable.GetUnderlyingType(propertyInfo.PropertyType) ?? propertyInfo.PropertyType;
+                propertyInfo.SetValue(obj, (val == null) ? null : Convert.ChangeType(val, type), null);
+            }
+        }
+    }
+}
---
+++ HomeProject.Backend.DAL/EFContextFactory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using HomeProject.Backend.Models.DB;
+using System.Collections.Concurrent;
+
+namespace HomeProject.Backend.DAL
+{
+    public class EFContextFactory
+    {
+        /// <summary>
+        /// 提供跨线程的实例访问
+        /// </summary>
+        static ConcurrentDictionary<string, AsyncLocal<object>> state = new ConcurrentDictionary<string, AsyncLocal<object>>();
+        //private static Hashtable cache = Hashtable.Synchronized(new Hashtable());//缓存
+        /// <summary>
+        /// 帮我们返回当前线程内的数据库上下文，如果当前线程内没有上下文，那么创建一个上下文，并保证
+        /// 上下文是实例在线程内部唯一
+        /// 在EF4.0以前使用ObjectsContext对象
+        /// </summary>
+        /// <returns></returns>
+        public static DbContext GetCurrentDbContext()
+        {
+            //当第二次执行的时候直接取出线程嘈里面的对象
+            //使用静态state对象作为线程内部唯一的独用的数据槽(一块内存空间)
+            //数据存储在线程栈中
+            //线程内共享一个单例
+            DbContext dbcontext = GetData("DbContext") as DbContext;
+
+            //判断线程里面是否有数据
+            if (dbcontext == null)  //线程的数据槽里面没有次上下文
+            {
+                dbcontext = new HomeDbContext();
+
+                //存储指定对象
+                SetData("DbContext", dbcontext);
+            }
+            return dbcontext;
+        }
+        /// <summary>
+        /// Stores a given object and associates it with the specified name.
+        /// </summary>
+        /// <param name="name">The name with which to associate the new item in the call context.</param>
+        /// <param name="data">The object to store in the call context.</param>
+        public static void SetData(string name, object data) =>
+            state.GetOrAdd(name, _ => new AsyncLocal<object>()).Value = data;
+
+        /// <summary>
+        /// Retrieves an object with the specified name from the <see cref="CallContext"/>.
+        /// </summary>
+        /// <param name="name">The name of the item in the call context.</param>
+        /// <returns>The object in the call context associated with the specified name, or <see langword="null"/> if not found.</returns>
+        public static object GetData(string name) =>
+            state.TryGetValue(name, out AsyncLocal<object> data) ? data.Value : null;
+    }
+}
